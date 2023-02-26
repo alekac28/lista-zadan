890dc840ca05cd2ba24b8954b33d7d5bc685114b
alekac28 <alekac28@st.amu.edu.pl>
2023-02-26 13:57:43 +0100

    Dodawanie, usuwanie, zatwierdzanie, liczenie wyk.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,25 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'lista-zadan';
+  newTask: string;
+  tasksList: Array<string> = [];
+  tasksDone: Array<string> = [];
+
+  add(){
+    this.tasksList.push(this.newTask);
+    this.newTask="";
+    console.log(this.tasksList);
+  }
+
+  remove(task: string){
+    // this.tasksList = this.tasksList.filter(e => e!==task);
+    const index: number = this.tasksList.indexOf(task);
+    this.tasksList.splice(index, 1);
+  }
+
+  done(task: string){
+    this.tasksDone.push(task);
+    this.remove(task);
+  }
+
 }
